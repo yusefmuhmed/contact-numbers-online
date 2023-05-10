4d155a43f0d412e8fc515b144f43a2799bde0c0d
yusefmuhmed <yusefmuhmed6766@gmail.com>
2023-05-10 22:50:59 +0300

    delete contact

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -2,13 +2,11 @@ const router = require("express").Router()
 const Contact = require('../app/controller/contact.controller')
 
 router.post("/addContact", Contact.addContact)
-router.get("/myPosts", Contact.myPosts)
+router.get("/fetchContacts", Contact.fetchContact)
 
-router.get("/all", Contact.getAll)
-router.delete("/all", Contact.deleteAll)
 
-router.get("/all/:id", Contact.getSingle)
-router.delete("/all/:id", Contact.deleteSingle)
-router.patch("/all/:id", Contact.editSingle)
+
+router.delete("/deleteContact/:phoneNum", Contact.deleteContact)
+router.put("/editContact/:id", Contact.editContact)
 
 module.exports = router
