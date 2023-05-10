3316c306ee1d4620f79aeb52270881b028975624
yusefmuhmed <yusefmuhmed6766@gmail.com>
2023-05-11 00:05:10 +0300

    "edit contact and fetch contact by limit" added

--- app/controller/contact.controller.js
+++ app/controller/contact.controller.js
@@ -30,13 +30,39 @@ class Contact {
     }
 
 
+    //fetch all contacts by limit = 5
+    static fetchContactLimit = async(req, res) => {
+        try {
+            const page = req.query.page ? parseInt(req.query.page) : 1;
+            const limit = 5;
+            const skip = (page - 1) * limit;
 
+            const contacts = await contactModel.find()
+                .skip(skip)
+                .limit(limit)
+            myHelper.resHandler(res, 200, true, contacts, "Contacts fetched successfully")
+        } catch (e) {
+            myHelper.resHandler(res, 500, false, e, e.message)
+        }
+
+    }
+
+    //filter contacts
+    static filterContact = async(req, res) => {
+        try {
+            const contacts = await contactModel.find()
+            myHelper.resHandler(res, 200, true, contacts, "Contacts fetched successfully")
+        } catch (e) {
+            myHelper.resHandler(res, 500, false, e, e.message)
+        }
+
+    }
 
     // delete contact
     static deleteContact = async(req, res) => {
         try {
             const phoneNum = req.params.phoneNum
-            const contact = await contactModel.findOneAndDelete({ phoneNum })
+            const contact = await contactModel.findOneAndRemove({ phoneNum })
             myHelper.resHandler(res, 200, true, contact, "Contact deleted successfully")
         } catch (e) {
             myHelper.resHandler(res, 500, false, e, e.message)
@@ -45,7 +71,17 @@ class Contact {
     }
 
     // edit contact
-    static editContact = async(req, res) => {}
+    static editContact = async(req, res) => {
+
+        try {
+            const { contactName, phoneNum, addresses, notes } = req.body;
+            const contact = await contactModel.findOneAndUpdate(req.params._id, { contactName, phoneNum, addresses, notes }, { new: true })
+            myHelper.resHandler(res, 200, true, contact, "Contact updated successfully")
+        } catch (e) {
+            myHelper.resHandler(res, 500, false, e, e.message)
+        }
+
+    }
 
 }
 module.exports = Contact
--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -4,9 +4,9 @@ const Contact = require('../app/controller/contact.controller')
 router.post("/addContact", Contact.addContact)
 router.get("/fetchContacts", Contact.fetchContact)
 
-
+router.get("/fetchContactsByLimit", Contact.fetchContactLimit)
 
 router.delete("/deleteContact/:phoneNum", Contact.deleteContact)
-router.put("/editContact/:id", Contact.editContact)
+router.put("/editContact/:phoneNum", Contact.editContact)
 
 module.exports = router
